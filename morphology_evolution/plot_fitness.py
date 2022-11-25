a9beb43e7dc030af3988ac65f326c5e5151799b2
CarloLonghi <carlo.longhi98@gmail.com>
2022-11-25 17:04:46 +0100

    plot correct generations

--- morphology_evolution/plot_fitness.py
+++ morphology_evolution/plot_fitness.py
@@ -1,44 +1,65 @@
 """Visualize and simulate the best robot from the optimization process."""
 
+import argparse
 import math
 import numpy as np
+import pandas
 
-from revolve2.core.database import open_async_database_sqlite
-from sqlalchemy.ext.asyncio.session import AsyncSession
+from revolve2.core.database import open_database_sqlite
 from sqlalchemy.future import select
 from revolve2.core.optimization.ea.generic_ea._database import (
     DbBase,
+    DbEAOptimizer,
     DbEAOptimizerIndividual,
+    DbEAOptimizerGeneration
 )
-from revolve2.core.database.serializers import FloatSerializer
-from array_genotype.array_genotype import ArrayGenotypeSerializer as BrainSerializer, develop as brain_develop
-from revolve2.genotypes.cppnwin.modular_robot.body_genotype_v1 import develop_v1 as body_develop
-from revolve2.genotypes.cppnwin._genotype import GenotypeSerializer as BodySerializer
+from revolve2.core.database.serializers import FloatSerializer, DbFloat
 from matplotlib import pyplot as plt
+from revolve2.core.optimization import DbId
 
 
-async def main() -> None:
+
+def plot(database: str, db_id: DbId) -> None:
 
     """Run the script."""
-    db = open_async_database_sqlite('database/')
-    async with AsyncSession(db) as session:
-        individuals = (
-            (
-                await session.execute(
-                    select(DbEAOptimizerIndividual.genotype_id, DbEAOptimizerIndividual.fitness_id)
-                )
+    db = open_database_sqlite(database)
+    df = pandas.read_sql(
+        select(
+            DbEAOptimizer,
+            DbEAOptimizerGeneration,
+            DbEAOptimizerIndividual,
+            DbFloat,
+        ).filter(
+            (DbEAOptimizer.db_id == db_id.fullname)
+            & (DbEAOptimizerGeneration.ea_optimizer_id == DbEAOptimizer.id)
+            & (DbEAOptimizerIndividual.ea_optimizer_id == DbEAOptimizer.id)
+            & (DbEAOptimizerIndividual.fitness_id == DbFloat.id)
+            & (
+                DbEAOptimizerGeneration.individual_id
+                == DbEAOptimizerIndividual.individual_id
             )
-            .all()
-        )
+        ),
+        db,
+    )
 
-        fitnesses_ids = [ind[1] for ind in individuals]
-        fitnesses = np.array([(await FloatSerializer.from_database(session, [id]))[0] for id in fitnesses_ids])
-
-    plt.plot(fitnesses)
+    fitnesses = df[['generation_index','individual_index','individual_id', 'value']].sort_values(by=["generation_index", "individual_index"])
+    plt.plot(np.linspace(start=1, stop=fitnesses.shape[0], num=fitnesses.shape[0]), fitnesses['value'])
     plt.show()
 
 
-if __name__ == "__main__":
-    import asyncio
 
-    asyncio.run(main())
+def main() -> None:
+    """Run this file as a command line tool."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "database",
+        type=str,
+        help="The database to plot.",
+    )
+    parser.add_argument("db_id", type=str, help="The id of the ea optimizer to plot.")
+    args = parser.parse_args()
+
+    plot(args.database, DbId(args.db_id))
+
+if __name__ == "__main__":
+    main()
